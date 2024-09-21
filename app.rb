43a9ce5726c9e2adebbb20dfbf842cf74daee36f
Murtaza Hiraj <murtazahiraj@berkeley.edu>
2024-09-21 18:21:22 +0000

    added rerun and updated ruby

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
   get '/' do
-    "<!DOCTYPE html><html><head></head><body><h1>Goodbye World</h1></body></html>"
+    "<!DOCTYPE html><html><head></head><body><h1>Why World?</h1></body></html>"
   end
 end
 
